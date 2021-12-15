4c64b6daa23b84a4a0a0f47c809634a9102d0f95
jean-benoit daille <68898672+jeanbenoitdaille@users.noreply.github.com>
2021-12-15 14:16:13 +0100

    Add files via upload

---
+++ index.cs
@@ -0,0 +1,14 @@
+    using System;
+     
+    namespace Coding.Exercise
+    {
+        public class Exercise
+        {
+            public static string[] Main() {
+                
+                var foo = new String[] {"127", "True", "Hello"};
+                
+                return foo;
+            }
+        }
+    }
